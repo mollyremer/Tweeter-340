ed7d312aeac00be45bd6fe471bdbb264264079e1
mollyremer <molly2434@gmail.com>
2024-03-04 10:48:19 -0700

    bug fix

--- tweeter-web/src/presenter/feedAndStory/FeedPresenter.ts
+++ tweeter-web/src/presenter/feedAndStory/FeedPresenter.ts
@@ -1,12 +1,12 @@
 import { AuthToken, Status, User } from "tweeter-shared";
-import { StatusItemPresenter, StatusItemView } from "./StatusItemPresenter";
+import { StatusItemPresenter } from "./StatusItemPresenter";
 import { PAGE_SIZE } from "./PagedItemPresenter";
 
 export class FeedPresenter extends StatusItemPresenter {
     protected getItemDescription(): string {
         return "load feed items";
     }
-    
+
     protected getMoreItems(authToken: AuthToken, user: User): Promise<[Status[], boolean]> {
         return this.service.loadMoreStoryItems(authToken, user, PAGE_SIZE, this.lastItem);
     }
--- tweeter-web/src/presenter/feedAndStory/StoryPresenter.ts
+++ tweeter-web/src/presenter/feedAndStory/StoryPresenter.ts
@@ -4,7 +4,7 @@ import { PAGE_SIZE } from "./PagedItemPresenter";
 
 export class StoryPresenter extends StatusItemPresenter {
     protected getItemDescription(): string {
-        throw new Error("Method not implemented.");
+        return "load story items";
     }
     protected getMoreItems(authToken: AuthToken, user: User): Promise<[Status[], boolean]> {
         return this.service.loadMoreStoryItems(authToken, user, PAGE_SIZE, this.lastItem);
