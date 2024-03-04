b9a812dfc740ace5042f51b60a4f7c963c1e04d8
mollyremer <molly2434@gmail.com>
2024-03-04 12:31:41 -0700

    register bug fix

--- tweeter-web/src/components/authentication/register/Register.tsx
+++ tweeter-web/src/components/authentication/register/Register.tsx
@@ -15,8 +15,8 @@ const Register = () => {
   const [lastName, setLastName] = useState("");
   const [alias, setAlias] = useState("");
   const [password, setPassword] = useState("");
-  const [imageBytes, setImageBytes] = useState<Uint8Array>(new Uint8Array());
-  const [imageUrl, setImageUrl] = useState<string>("");
+  const [imageBytes] = useState<Uint8Array>(new Uint8Array());
+  const [imageUrl] = useState<string>("");
   const [rememberMe, setRememberMe] = useState(false);
 
   const rememberMeRef = useRef(rememberMe);
@@ -44,29 +44,7 @@ const Register = () => {
   };
 
   const handleImageFile = (file: File | undefined) => {
-    if (file) {
-      setImageUrl(URL.createObjectURL(file));
-
-      const reader = new FileReader();
-      reader.onload = (event: ProgressEvent<FileReader>) => {
-        const imageStringBase64 = event.target?.result as string;
-
-        // Remove unnecessary file metadata from the start of the string.
-        const imageStringBase64BufferContents =
-          imageStringBase64.split("base64,")[1];
-
-        const bytes: Uint8Array = Buffer.from(
-          imageStringBase64BufferContents,
-          "base64"
-        );
-
-        setImageBytes(bytes);
-      };
-      reader.readAsDataURL(file);
-    } else {
-      setImageUrl("");
-      setImageBytes(new Uint8Array());
-    }
+    presenter.handleImageFile(file, imageUrl, imageBytes);
   };
 
   const doRegister = async () => {
--- tweeter-web/src/presenter/authentication/RegisterPresenter.ts
+++ tweeter-web/src/presenter/authentication/RegisterPresenter.ts
@@ -1,4 +1,5 @@
 import { AuthenticatePresenter, AuthenticateView } from "./AuthenticatePresenter";
+import { Buffer } from "buffer";
 
 export class RegisterPresenter extends AuthenticatePresenter {
     constructor(view: AuthenticateView) {
@@ -15,5 +16,31 @@ export class RegisterPresenter extends AuthenticatePresenter {
     protected navigateTo(): void {
         this.view.navigate("/");
     }
+
+    public handleImageFile(file: File | undefined, imageURL: string, imageBytes: Uint8Array){
+        if (file) {
+          imageURL = URL.createObjectURL(file);
+    
+          const reader = new FileReader();
+          reader.onload = (event: ProgressEvent<FileReader>) => {
+            const imageStringBase64 = event.target?.result as string;
+    
+            // Remove unnecessary file metadata from the start of the string.
+            const imageStringBase64BufferContents =
+              imageStringBase64.split("base64,")[1];
+    
+            const bytes: Uint8Array = Buffer.from(
+              imageStringBase64BufferContents,
+              "base64"
+            );
+    
+            imageBytes = bytes;
+          };
+          reader.readAsDataURL(file);
+        } else {
+          imageURL = "";
+          imageBytes = new Uint8Array();
+        }
+      };
 
 }
